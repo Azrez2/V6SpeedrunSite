fd8eaba0a1ac6938180d06727e5d396c89f17d8f
Azrez <41131417+Azrez2@users.noreply.github.com>
2022-06-03 22:17:51 -0600

    create index.php
    Basic mockup of site, will have to figure out implementation of subcategories later, not connected to db yet but will implement that with php

---
+++ index.php
@@ -0,0 +1,69 @@
+<html>
+	<head>
+		<style>
+			td, th {
+				border: 1px solid #dddddd;
+				text-align: left;
+				padding: 8px;
+			}
+			section {
+				display: none;
+			}
+
+			section:target {
+				display: block;
+			}
+
+			section#nooob { /* Show #nooob by default */
+				display: block;
+			}
+		</style>
+	</head>
+	
+	<body>
+		
+		<h1>The VVVVVV Speedrunning Leaderboard (WIP)</h1>
+		<p>I got it working I think</p>
+		
+		<nav>
+			<a href="#nooob">No OOB</a>
+			<a href="#glitchlessany">Glitchless Any%</a>
+		</nav>
+		
+		<br>
+		
+		<section id="nooob">
+			<table>
+				<tr>
+					<th>Rank</th>
+					<th>Player</th>
+					<th>In-game time</th>
+					<th>Platform</th>
+					<th>Route</th>
+					<th>Date</th>
+				</tr>
+				<tr>
+					<td>1</td>
+					<td>Elomavi</td>
+					<td>12:00.400</td>
+					<td>PC</td>
+					<td>Gravitron Skip</td>
+					<td>3 months ago</td>
+				</tr>
+				<tr>
+					<td>2</td>
+					<td>kss1</td>
+					<td>12:14.960</td>
+					<td>PC</td>
+					<td>Gravitron Skip</td>
+					<td>2 months ago</td>
+				</tr>
+			</table>
+		</section>
+		
+		<section id="glitchlessany">
+			<p>Glitchless Any% Leaderboards would go here</p>
+		</section>
+		
+	</body>
+<html>
